46037378cf8537414c0589bc8385b7ae90df80fe
melikaeslahi <melika.eslahi@gmail.com>
2024-07-04 22:03:09 +0330

    change title name

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,7 +11,7 @@ const Products =() =>{
     return(
     <>
     <section className="flex flex-col items-center justify-center mt-5 ">
-     <h1 className="text-red-600 font-bold text-3xl p-5">همه ی محصولات</h1>
+     <h1 className="text-red-600 font-bold text-3xl p-5">All Products</h1>
     
      <section className="container w-3/4 flex justify-around items-center flex-wrap" >
       
